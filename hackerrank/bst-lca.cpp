790a6d54352583bba2da794727ad09aa67edf77e
Shekhar Prasad Rajak <shekharstudy@ymail.com>
2015-07-21 13:18:21 +0530

    hackerrank

---
+++ hackerrank/bst-lca.cpp
@@ -0,0 +1,7 @@
+if(!cur_root)
+		return -1;//error
+	if(v1<cur_root->v && v2<cur_root->v)
+		return bst_lca(cur_root->left,v1,v2);
+	else if(v1>cur_root->v && v2>cur_root->v)
+		return bst_lca(cur_root->right,v1,v2);
+	else return cur_root->v;
