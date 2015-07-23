0fb3dd56e32e07b7892ff3983c899fe5a76df85a
Shekhar Prasad Rajak <shekharstudy@ymail.com>
2015-07-23 12:41:49 +0530

    hackerrank lca

---
+++ leetcode/33problme.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#define x 5+2
+
+/*
+	Name: 
+	Copyright: 
+	Author: 
+	Date: 22/07/15 18:45
+	Description: 
+*/
+
+int main(){
+
+int i=320;
+char *ptr=(char *)&i;
+printf("%d",*ptr);
+
+}
