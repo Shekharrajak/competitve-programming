316d42587081ec1261057a112f1202a1904e2b40
Shekhar Prasad Rajak <shekharstudy@ymail.com>
2015-07-25 23:44:12 +0530

    ambi

---
+++ interview ques.cpp
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+using namespace std;
+int main(){
+	long long int time;
+	cout<<"Enter the time after which pc should shut down :"<<endl;
+	cin>>time;
+	while(time--){
+		system("explorer C:\\user\\desktop\\shutdown_icon"); //the shortcut which contains :"  shutdown -s -t 0 "
+	}
+
+return 2;
+}
