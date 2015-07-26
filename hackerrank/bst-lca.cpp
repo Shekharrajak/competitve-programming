779ed7e0007c906411b834b44874f34db293a727
Shekhar Prasad Rajak <shekharstudy@ymail.com>
2015-07-27 00:22:30 +0530

    Update bst-lca.cpp

--- hackerrank/bst-lca.cpp
+++ hackerrank/bst-lca.cpp
@@ -1,3 +1,5 @@
+int bst_lca(node* cur_root,int v1,int v3)
+{
 if(!cur_root)
 		return -1;//error
 	if(v1<cur_root->v && v2<cur_root->v)
@@ -5,3 +7,4 @@ if(!cur_root)
 	else if(v1>cur_root->v && v2>cur_root->v)
 		return bst_lca(cur_root->right,v1,v2);
 	else return cur_root->v;
+}
