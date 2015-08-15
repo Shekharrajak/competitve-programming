e424c2d8512537a7440cd547d7b88433bd38e6da
shekharrajak <shekharstudy@ymail.com>
2015-08-15 14:22:38 +0530

    GamePickFromEndOptimal.cpp

--- GamePickFromEndOptimal/main.cpp
+++ GamePickFromEndOptimal/main.cpp
@@ -9,15 +9,92 @@ N pots, each with some number of gold coins, are arranged in a line. You are pla
 #include<bits/stdc++.h>
 using namespace std;
 
+
+pair<int ,int> dp(int val[]){
+    vector<vector<pair<int,int> > > vec;
+
+    int s =sizeof(val)/sizeof(val[0]);
+    int l=1;
+    while(l<s+1){
+        if(l==1){
+             for(int i =1;i<=s ;i++){
+        vec[i][i].first = val[i];
+        vec[i][i].second = 0;
+
+        }
+        cout<<endl<<endl;
+         for(int i =1;i<=s ;i++){
+            for(int j=1;j<=s ; j++){
+                cout<<vec[i][j].first<<","<<vec[i][j].second<<"  ";
+            }
+            cout<<endl;
+
+        }
+
+    }
+        else{
+            for(int i =1;;i++){
+                vec[i][i+l-1].first = max(
+                                    (vec[i][i].first + vec[i+1][i+l-1].second),
+                                    (vec[i+l-1][i+l-1].first + vec[i][i+l-2].second)
+                                  );
+                 vec[i][i+l-1].second = (vec[i][i+l-1].first == (vec[i][i].first + vec[i+1][i+l-1].second) ? vec[i+1][i+l-1].first : vec[i][i+l-2].first);
+                if((i+l-1 )== s){
+                    break;
+                }
+            }
+        }
+        l++;
+    }
+            cout<<endl<<endl;
+         for(int i =1;i<=s ;i++){
+            for(int j=1;j<=s ; j++){
+                cout<<vec[i][j].first<<","<<vec[i][j].second<<"  ";
+            }
+            cout<<endl;
+
+        }
+
+    return vec[1][s];
+}
+int main()
+{
+    int pots,temp;
+    cin>>pots;
+
+    int *val;
+
+    for(int i =1;i<=pots;i++){
+        cin>>temp;
+        val[i] = temp;;
+    }
+    for(int i =1;i<=pots;i++){
+        cout<<val[i];
+    }
+    pair<int,int> ans = dp(val);
+    cout<<ans.first <<" "<<ans.second;
+
+}
+
+
+/*
 pair<int ,int> dp(vector<int> val){
     vector<pair<int,int> > vec[val.size()+1];
 
     int l=1;
-    while(l<val.size()+1){
+    while(l<(int)val.size()+1){
         if(l==1){
              for(int i =1;i<=val.size() ;i++){
         vec[i][i].first = val[i];
         vec[i][i].second = 0;
+
+        }
+        cout<<endl<<endl;
+         for(int i =1;i<=val.size() ;i++){
+            for(int j=1;j<=val.size() ; j++){
+                cout<<vec[i][j].first<<","<<vec[i][j].second<<"  ";
+            }
+            cout<<endl;
 
         }
 
@@ -34,19 +111,40 @@ pair<int ,int> dp(vector<int> val){
                 }
             }
         }
+        l++;
     }
-    vec[1][val.size()+1];
+            cout<<endl<<endl;
+         for(int i =1;i<=val.size() ;i++){
+            for(int j=1;j<=val.size() ; j++){
+                cout<<vec[i][j].first<<","<<vec[i][j].second<<"  ";
+            }
+            cout<<endl;
+
+        }
+
+    return vec[1][val.size()];
 }
 int main()
 {
-    int pots;
+    int pots,temp;
     cin>>pots;
 
     vector<int> val;
+
     for(int i =1;i<=pots;i++){
-        cin>>val[i];
+        cin>>temp;
+        val.push_back(temp);
+    }
+    vector<int> :: iterator itr = val.begin();
+    while(itr != val.end()){
+        cout<<*itr<<" ";
+        itr++;
+    }
+    for(int i =1;i<=pots;i++){
+        cout<<val[i];
     }
     pair<int,int> ans = dp(val);
     cout<<ans.first <<" "<<ans.second;
 
 }
+*/
